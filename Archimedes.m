63b280f4aa8316123a569b9075a9ac0709b50f96
felixjohanlehmann <42647708+felixjohanlehmann@users.noreply.github.com>
2018-12-09 11:09:49 +0100

    Upload of latest version

---
+++ Archimedes.m
@@ -0,0 +1,162 @@
+ergebnis=[];
+z=0;
+for a=1:19
+    for b=1:19
+        if isequal(a,b)
+            continue
+        else
+        c=38-a-b;
+        if c>19|c<1
+            continue
+        else
+        if isequal(a,c)|isequal(b,c)
+            continue
+        else
+            for d=1:19
+                if isequal(a,d)|isequal(b,d)|isequal(c,d)
+                    continue
+                else
+                for e=1:19
+                    if isequal(a,e)|isequal(b,e)|isequal(c,e)|isequal(d,e)
+                        continue
+                    else                    
+                    for f=1:19
+                        if isequal(a,f)|isequal(b,f)|isequal(c,f)|isequal(d,f)|isequal(e,f)
+                            continue
+                        else
+                        for g=1:19
+                            if isequal(a,g)|isequal(b,g)|isequal(c,g)|isequal(d,g)|isequal(e,g)|isequal(f,g)
+                                continue
+                            else
+                            h=38-a-d;
+                            if h>19|h<1
+                                continue
+                            else
+                            if isequal(a,h)|isequal(b,h)|isequal(c,h)|isequal(d,h)|isequal(e,h)|isequal(f,h)|isequal(g,h)
+                                continue
+                            else
+                                for i=1:19
+                                    if isequal(a,i)|isequal(b,i)|isequal(c,i)|isequal(d,i)|isequal(e,i)|isequal(f,i)|isequal(g,i)|isequal(h,i)
+                                        continue
+                                    else
+                                    for j=1:19
+                                        if isequal(a,j)|isequal(b,j)|isequal(c,j)|isequal(d,j)|isequal(e,j)|isequal(f,j)|isequal(g,j)|isequal(h,j)|isequal(i,j)
+                                            continue
+                                        else
+                                        for k=1:19
+                                            if isequal(a,k)|isequal(b,k)|isequal(c,k)|isequal(d,k)|isequal(e,k)|isequal(f,k)|isequal(g,k)|isequal(h,k)|isequal(i,k)|isequal(j,k)
+                                                continue
+                                            else
+                                            l=38-c-g;
+                                            if l>19|l<1
+                                                continue
+                                            else
+                                            if isequal(a,l)|isequal(b,l)|isequal(c,l)|isequal(d,l)|isequal(e,l)|isequal(f,l)|isequal(g,l)|isequal(h,l)|isequal(i,l)|isequal(j,l)|isequal(k,l)
+                                                continue
+                                            else    
+                                                m=38-b-e-i;
+                                                if m>19|m<1
+                                                    continue
+                                                else
+                                                if isequal(a,m)|isequal(b,m)|isequal(c,m)|isequal(d,m)|isequal(e,m)|isequal(f,m)|isequal(g,m)|isequal(h,m)|isequal(i,m)|isequal(j,m)|isequal(k,m)|isequal(l,m)
+                                                    continue
+                                                else
+                                                    for n=1:19
+                                                        if isequal(a,n)|isequal(b,n)|isequal(c,n)|isequal(d,n)|isequal(e,n)|isequal(f,n)|isequal(g,n)|isequal(h,n)|isequal(i,n)|isequal(j,n)|isequal(k,n)|isequal(l,n)|isequal(m,n)
+                                                            continue
+                                                        else
+                                                        for o=1:19
+                                                            if isequal(a,o)|isequal(b,o)|isequal(c,o)|isequal(d,o)|isequal(e,o)|isequal(f,o)|isequal(g,o)|isequal(h,o)|isequal(i,o)|isequal(j,o)|isequal(k,o)|isequal(l,o)|isequal(m,o)|isequal(n,o)
+                                                                continue
+                                                            else
+                                                            p=38-b-f-k;
+                                                            if p>19|p<1
+                                                                continue
+                                                            else
+                                                            if isequal(a,p)|isequal(b,p)|isequal(c,p)|isequal(d,p)|isequal(e,p)|isequal(f,p)|isequal(g,p)|isequal(h,p)|isequal(i,p)|isequal(j,p)|isequal(k,p)|isequal(l,p)|isequal(m,p)|isequal(n,p)|isequal(o,p)
+                                                                continue
+                                                            else
+                                                                q=38-h-m;
+                                                                if q>19|q<1
+                                                                    continue
+                                                                else
+                                                                if isequal(a,q)|isequal(b,q)|isequal(c,q)|isequal(d,q)|isequal(e,q)|isequal(f,q)|isequal(g,q)|isequal(h,q)|isequal(i,q)|isequal(j,q)|isequal(k,q)|isequal(l,q)|isequal(m,q)|isequal(n,q)|isequal(o,q)|isequal(p,q)
+                                                                    continue
+                                                                else
+                                                                    r=38-d-i-n;
+                                                                    if r>19|r<1
+                                                                        continue
+                                                                    else
+                                                                    if isequal(a,r)|isequal(b,r)|isequal(c,r)|isequal(d,r)|isequal(e,r)|isequal(f,r)|isequal(g,r)|isequal(h,r)|isequal(i,r)|isequal(j,r)|isequal(k,r)|isequal(l,r)|isequal(m,r)|isequal(n,r)|isequal(o,r)|isequal(p,r)|isequal(q,r)
+                                                                        continue
+                                                                    else
+                                                                        s=38-l-p;
+                                                                        if s>19|s<1
+                                                                            continue
+                                                                        else
+                                                                        if isequal(a,s)|isequal(b,s)|isequal(c,s)|isequal(d,s)|isequal(e,s)|isequal(f,s)|isequal(g,s)|isequal(h,s)|isequal(i,s)|isequal(j,s)|isequal(k,s)|isequal(l,s)|isequal(m,s)|isequal(n,s)|isequal(o,s)|isequal(p,s)|isequal(q,s)|isequal(r,s)
+                                                                            continue
+                                                                        else    
+                                                                        z=z+1;
+
+
+
+% Bilde Matrix X, die das Sechseck repräsentiert
+                        X= [a b c 0 0; d e f g 0; h i j k l; 0 m n o p; 0 0 q r s];
+% Bilde Zeilen-, Spalten- und Diagonalensummen 
+                        S=sum(X);
+                        T=sum(X');
+                        U=trace(X);
+                        V=sum(diag(X,1));
+                        W=sum(diag(X,2));
+                        B=sum(diag(X,-1));
+                        Y=sum(diag(X,-2));
+% Vergleiche obige Summen mit Zielwert (38) und Beendung
+% andernfalls Fortsetzung
+                        A=[U V W B Y];
+                        Z=[ 38 38 38 38 38];
+                       
+                        
+                    if  isequal(Z,S,T,A)
+                        ergebnis=[ergebnis;X];
+                        return
+                    else
+                        continue
+                    end
+                                                                        end
+                                                                    end
+                                                                end
+                                                            end
+                                                            end
+                                                        end
+                                                        end
+                                                    end
+                                                end
+                                            end
+                                            end
+                                        end
+                                        end
+                                    end
+                                    end
+                                end
+                            end
+                            end
+                        end
+                        end
+                    end
+                    end
+                end
+                end
+            end
+        end
+        end
+    end
+end
+                end
+                end
+            end
+        end
+        end
+        end
+    end
+end
